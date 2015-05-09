b890c6f6af2f334a66af3b524a237ddabc5306eb
Daniel Ménard <daniel.menard@laposte.net>
2015-05-09 16:29:13 +0200

    UserData : améliorations
    - userId() : retourne 0 plutôt que de générer une exception lorsque
    l'utilisateur en cours n'est pas connecté.
    - get() : retourne null si l'utilisateur n'est pas connecté plutôt que
    de créer un objet vide.
    - basket() : retourne le panier en cours lorsqu'aucun argument n'est
    indiqué et permet d'avoir plusieurs paniers (le panier en cours peut
    être indiqué dans le cookie).
    - ajout de phpdoc

--- class/UserData.php
+++ class/UserData.php
@@ -12,6 +12,7 @@
  * @author      Daniel Ménard <daniel.menard@laposte.net>
  * @version     SVN: $Id$
  */
+
 namespace Docalist\Biblio\UserData;
 
 use WP_User;
@@ -76,7 +77,7 @@ class UserData {
             $user = get_user_by('login', $user);
         }
 
-        if (! $user instanceof WP_User || ! $user->exists()) {
+        if (! $user instanceof WP_User) {
             throw new InvalidArgumentException('Invalid user');
         }
 
@@ -135,23 +136,61 @@ class UserData {
      * @return UserDataObject
      */
     protected function get($type, $name, $user = null) {
-        if (!isset($this->instances[$type][$name])) {
-            $class = $this->className($type);
+        if (!isset($this->instances[$type]) || ! array_key_exists($name, $this->instances[$type])) {
             $user = $this->userID($user);
-            $this->instances[$type][$name] = new $class($name, $user);
+            if ($user === 0) {
+                $this->instances[$type][$name] = null;
+            } else {
+                $class = $this->className($type);
+                $this->instances[$type][$name] = new $class($name, $user);
+            }
         }
 
         return $this->instances[$type][$name];
     }
 
     /**
+     * Retourne un panier.
+     *
+     * Sans arguments, la méthode retourne le panier en cours de l'utilisateur
+     * en cours, ce qui est l'usage le plus courant.
+     *
+     * Si l'utilisateur n'est pas connecté, la méthode retourne null. Dans le
+     * cas contraire, la méthode regarde si un numéro de panier figure dans le
+     * cookie et le prends en compte. Si le cookie n'existe pas ou n'est pas
+     * correct, retourne le panier par défaut qui s'appelle "1".
      *
      * @param string $name
-     * @param int|string|WP_User L'utilsateur (null = utilisateur en cours).
+     * @param int|string|WP_User L'utilisateur (null = utilisateur en cours).
      *
      * @return Basket
      */
-    public function basket($name, $user = null) {
+    public function basket($name = null, $user = null) {
+        // Si name est à null, détermine le nom du panier en cours
+        if (is_null($name)) {
+            // Sanity check
+            if (! is_null($user)) {
+                throw new InvalidArgumentException('If name is null, user must also be null');
+            }
+
+            // Le panier par défaut s'appelle "1"
+            $name = 1;
+
+            // Si on a cookie avec un autre nom, on le prend après validation
+            if (isset($_COOKIE['basket'])) {
+                $options = [
+                    'flags' => FILTER_NULL_ON_FAILURE,
+                    'options' => [
+                        'min_range' => 1,
+                        'max_range' => 10, // 10 paniers, c'est déjà pas mal, non ?
+                        'default'   => $name
+                    ]
+                ];
+
+                $name = filter_var($_COOKIE['basket'], FILTER_VALIDATE_INT, $options);
+            }
+        }
+
         return $this->get('basket', $name, $user);
     }
 }
