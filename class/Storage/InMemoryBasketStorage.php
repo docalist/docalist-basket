ecea37dc6cfec99c2704d6c9588f0380932c5879
Daniel Ménard <daniel.menard@laposte.net>
2018-07-17 11:57:33 +0200

    Nouvelle classe Storage/InMemoryBasketStorage
    Stockage des paniers en mémoire (pour les tests).

---
+++ class/Storage/InMemoryBasketStorage.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of Docalist Basket.
+ *
+ * Copyright (C) 2015-2018 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+namespace Docalist\Basket\Storage;
+
+use Docalist\Basket\Storage;
+
+/**
+ * Stockage des paniers en mémoire (pour les tests).
+ *
+ * @author Daniel Ménard <daniel.menard@laposte.net>
+ */
+class InMemoryBasketStorage implements Storage
+{
+    private $baskets = [];
+
+    public function loadBasketList(): array
+    {
+        return $this->baskets['list'] ?? [];
+    }
+
+    public function saveBasketList(array $list): void
+    {
+        $this->baskets['list'] = $list;
+    }
+
+    public function loadBasketData(int $basket): array
+    {
+        return $this->baskets[$basket] ?? [];
+    }
+
+    public function saveBasketData(int $basket, array $data): void
+    {
+        $this->baskets[$basket] = $data;
+    }
+}
