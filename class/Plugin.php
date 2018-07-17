de83aa71835b38aaa60b6fc39a7429606df160ca
Daniel Ménard <daniel.menard@laposte.net>
2018-07-17 12:01:23 +0200

    Plugin : adaptation renommage

--- class/Plugin.php
+++ class/Plugin.php
@@ -1,62 +1,54 @@
 <?php declare(strict_types=1);
 /**
- * This file is part of Docalist UserData.
+ * This file is part of Docalist Basket.
  *
  * Copyright (C) 2015-2018 Daniel Ménard
  *
  * For copyright and license information, please view the
  * LICENSE file that was distributed with this source code.
+ */
+namespace Docalist\Basket;
+
+use Docalist\Basket\Settings\BasketSettings;
+use Docalist\Views;
+use Docalist\Basket\AdminPage\BasketSettingsPage;
+use Docalist\Basket\Api\BasketService;
+use Docalist\Basket\Widget\BasketWidget;
+
+/**
+ * Plugin docalist-basket.
  *
  * @author Daniel Ménard <daniel.menard@laposte.net>
  */
-namespace Docalist\UserData;
-
-use Docalist\Views;
-
 class Plugin
 {
-    /**
-     * Les paramètres du plugin.
-     *
-     * @var Settings
-     */
-    protected $settings;
-
     public function __construct()
     {
         // Charge les fichiers de traduction du plugin
-        load_plugin_textdomain('docalist-userdata', false, 'docalist-userdata/languages');
-
-        // Ajoute notre répertoire "views" au service "docalist-views"
-        add_filter('docalist_service_views', function(Views $views) {
-            return $views->addDirectory('docalist-userdata', DOCALIST_USERDATA_DIR . '/views');
-        });
+        load_plugin_textdomain('docalist-basket', false, 'docalist-basket/languages');
 
         // Charge la configuration du plugin
-        $this->settings = new Settings(docalist('settings-repository'));
+        $settings = new BasketSettings(docalist('settings-repository'));
+
+        // Crée le service docalist "basket"
+        docalist('services')->add('basket', function () use ($settings) {
+            return new BasketService($settings);
+        });
+
+        // Ajoute notre répertoire "views" au service "docalist-views"
+        add_filter('docalist_service_views', function (Views $views) {
+            return $views->addDirectory('docalist-basket', DOCALIST_BASKET_DIR . '/views');
+        });
 
         // Crée la page de réglages du plugin
-        add_action('admin_menu', function () {
-            new SettingsPage($this->settings);
+        add_action('admin_menu', function () use ($settings) {
+            new BasketSettingsPage($settings);
         });
 
         // Déclare le widget "Basket"
         add_action('widgets_init', function () {
-            register_widget('Docalist\UserData\BasketWidget');
+            register_widget(BasketWidget::class);
         });
-
-        // Si l'utilisateur en cours n'est pas connecté, aucun service n'est créé
-        if (! is_user_logged_in()) {
-            return;
-        }
-
-        // Créée le service de gestion des données utilisateur
-        docalist('services')->add('user-data', function () {
-            return new UserData();
-        });
-
-        // Crée les actions ajax pour le panier
-        docalist('services')->add('basket-controller', new BasketController($this->settings));
 
         // Déclare nos assets
         require_once dirname(__DIR__) . '/assets/register.php';
