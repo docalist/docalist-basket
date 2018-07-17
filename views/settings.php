5f2e6e9d5f52b2c8bee1176fab6a147e89a1cb69
Daniel Ménard <daniel.menard@laposte.net>
2018-07-17 12:38:10 +0200

    view/settings : ré-écriture complète
    - déplace dans /views (dans /views/settings avant)
    - laisse les Settings générer le formulaire

---
+++ views/settings.php
@@ -0,0 +1,39 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of Docalist Basket.
+ *
+ * Copyright (C) 2015-2018 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+namespace Docalist\Basket\Views;
+
+use Docalist\Basket\AdminPage\BasketSettingsPage;
+use Docalist\Basket\Settings\BasketSettings;
+use Docalist\Forms\Form;
+
+/**
+ * Page de configuration du module panier.
+ *
+ * @var BasketSettingsPage  $this       La page de configuration du panier.
+ * @var BasketSettings      $settings   Les paramètres du panier.
+ *
+ * @author Daniel Ménard <daniel.menard@laposte.net>
+ */
+?>
+<div class="wrap">
+    <h1><?= __('Panier Docalist', 'docalist-basket') ?></h1>
+
+    <p class="description"><?php
+        _e("Le panier Docalist permet de sélectionner et d'exploiter des listes de notices.", 'docalist-basket');
+    ?></p>
+
+    <?php
+        $form = new Form();
+        $form->addItems($this->settings->getEditorForm()->getItems());
+        $form->submit(__('Enregistrer les modifications', 'docalist-basket'))
+            ->addClass('button button-primary');
+        $form->bind($settings)->display('wordpress');
+    ?>
+</div>
