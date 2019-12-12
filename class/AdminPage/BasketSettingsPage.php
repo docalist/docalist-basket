30f86b3832f7aaa14005c4bd8c611ae7d07fb521
Daniel Ménard <daniel.menard@laposte.net>
2019-12-12 11:18:49 +0100

    MAJ année copyright

--- class/AdminPage/BasketSettingsPage.php
+++ class/AdminPage/BasketSettingsPage.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of Docalist Basket.
  *
- * Copyright (C) 2015-2018 Daniel Ménard
+ * Copyright (C) 2015-2019 Daniel Ménard
  *
  * For copyright and license information, please view the
  * LICENSE file that was distributed with this source code.
